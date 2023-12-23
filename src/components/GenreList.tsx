00a78df581dd76fb5d1bfa58a22982c2c20fb9f6
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2023-12-23 21:09:44 +0300

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,6 +4,7 @@ import useData from "../hooks/useData";
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -13,17 +14,18 @@ import {
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
-  onSelectedGenre : (genre : Genre) => void;
-  selectedGenre : Genre | null;
+  onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectedGenre , selectedGenre} : Props) => {
+const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
   if (isLoading) return <Spinner />;
   return (
     <>
+      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
@@ -31,10 +33,18 @@ const GenreList = ({onSelectedGenre , selectedGenre} : Props) => {
               <Image
                 boxSize="32px"
                 borderRadius={8}
+                objectFit="cover"
                 src={getCroppedImageUrl(genre.image_background)}
               />
 
-              <Button fontWeight={genre?.id === selectedGenre?.id ? 'bold' : 'normal' } fontSize="lg" variant="link" onClick={() => onSelectedGenre(genre)}>
+              <Button
+                whiteSpace={"normal"}
+                textAlign="left"
+                fontWeight={genre?.id === selectedGenre?.id ? "bold" : "normal"}
+                fontSize="lg"
+                variant="link"
+                onClick={() => onSelectedGenre(genre)}
+              >
                 {genre.name}
               </Button>
             </HStack>
