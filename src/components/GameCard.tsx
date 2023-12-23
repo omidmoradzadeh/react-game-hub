4e22062c874dff3600e8e10b49271a545db666cd
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2023-12-23 13:43:46 +0300

    Remove duplicatied styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
@@ -10,18 +11,20 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width='300px' borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
-      <CardBody>
-        <Heading fontSize="2xl"> {game.name}</Heading>
-        <HStack justifyContent="space-between">
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image src={game.background_image} />
+        <CardBody>
+          <Heading fontSize="2xl"> {game.name}</Heading>
+          <HStack justifyContent="space-between">
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,15 +1,18 @@
-import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import React from "react";
+import GameCardContainer from "./GameCardContainer";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width='300px' borderRadius={10} overflow="hidden">
-        <Skeleton height="200px"/>
+    <GameCardContainer>
+      <Card>
+        <Skeleton height="200px" />
         <CardBody>
-            <SkeletonText/>
+          <SkeletonText />
         </CardBody>
-    </Card>
-  )
-}
+      </Card>
+    </GameCardContainer>
+  );
+};
 
-export default GameCardSkeleton
+export default GameCardSkeleton;
