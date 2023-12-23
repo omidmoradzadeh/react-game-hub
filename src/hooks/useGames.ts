ced806aec1f8e63b3e1ecdfc2bfe82e88ccf965e
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2023-12-23 12:54:39 +0300

    Displaying platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,8 +6,14 @@ export interface Game {
   id: number;
   name: string;
   background_image : string
+  parent_platforms : {platform : Platform}[]
 }
 
+export interface Platform{
+  id : number, 
+  name : string ,
+  slug : string
+}
 interface FetchGamesResponse {
   count: number;
   results: [Game];
