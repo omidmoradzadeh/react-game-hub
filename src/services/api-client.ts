31635c393fbcca0b4f13e4e92ab260b9e08ac088
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2023-12-30 19:42:43 +0300

    reusable api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,30 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstanse = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "1d137f6245044bd5ad0739711f5689d8",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstanse
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+
+export default APIClient;
