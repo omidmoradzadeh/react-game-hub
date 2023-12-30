177a2ae1043406152e30f3c273b65ebf63345ebc
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2023-12-30 16:42:55 +0300

    Refactor Game hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,8 @@
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import { FetchResponse } from "./useData";
 import { Platform } from "./usePlatforms";
+import apiClient from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
 
 export interface Game {
   id: number;
@@ -13,17 +14,19 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery | null) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery?.genre?.id,
-        platforms: gameQuery?.platform?.id,
-        ordering: gameQuery?.sortOrder,
-        search: gameQuery?.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game> , Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery?.genre?.id,
+            parent_platforms: gameQuery?.platform?.id,
+            ordering: gameQuery?.sortOrder,
+            search: gameQuery?.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
