85c07622cf5f2b1eaaef603ef9fd4fc7c45eb8be
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2024-01-05 23:23:53 +0300

    Refactore - move entity interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,19 +1,8 @@
-import { Platform } from "./usePlatforms";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import useGameQueryStore from "../store";
-
-export interface Game {
-  id: number;
-  name: string;
-  slug : string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-  description_raw : string;
-}
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import platfoms from "../data/platfoms";
 import ms from "ms";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
 const usePlatforms = () =>
   useQuery({
     //useData<Platform>("/platforms/lists/parents");
