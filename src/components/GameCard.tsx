b290d621bc88a394102d8ed9169a790136cdc809
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2024-01-05 23:20:32 +0300

    Fetch game detail

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,6 +5,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import GameCardContainer from "./GameCardContainer";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -12,18 +13,21 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-      <Card>
-        <Image src={game.background_image} />
-        <CardBody>
-          <HStack justifyContent="space-between" marginBottom={3}>
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-          <Heading fontSize="2xl"> {game.name} <Emoji rating={game.rating_top}/></Heading>
-        </CardBody>
-      </Card>
+    <Card >
+      <Image src={game.background_image} />
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">
+          <Link to={"/games/"+game.slug}> {game.name} </Link>
+          <Emoji rating={game.rating_top} />
+        </Heading>
+      </CardBody>
+    </Card>
   );
 };
 
