6c1b09adc2f1513c804f3f6486f577025dfc069b
Omid Moradzadeh <omid.moradzadeh@aksis.com.tr>
2024-01-05 23:47:34 +0300

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,15 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
-
+import { Publishers } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers : Publishers[];
   background_image: string;
-  parent_platforms: { platform: Platform; }[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
   description_raw: string;
